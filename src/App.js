36ca0fc7dc126375221acf07775e5b1d8675ed89
aaron <binauralvoice@gmail.com>
2019-08-05 14:04:01 -0500

    Add SearchBar and VideoDetail components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 
 import { Grid } from '@material-ui/core';
-import { SearchBar, VideoList, VideoDetail } from './components';
+import { SearchBar, VideoDetail } from './components';
 
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: '[API KEY]',
+                q: searchTerm,
+            }
+        });
+
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+    }
+
     render () {
+        const { selectedVideo } = this.state;
         return(
-            <Grid justify='center' container spacing={16}>
+            <Grid justify='center' container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
-                            {/* SEARCH BAR */}
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs={8}>
-                            {/* VIDEO DETAIL */}
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
                             {/* VIDEO LIST */}
